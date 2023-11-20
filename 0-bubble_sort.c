95686e717b09d5db4e8de09fb0ea8e368300470e
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-20 13:36:49 +0200

    added task 0 fix 4

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,12 +25,11 @@ void bubble_sort(int *array, size_t size)
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
 					swap = true;
+					print_array(array, size);
 				}
 			}
 			if (swap == false)
 				break;
-			print_array(array, size);
-
 		}
 	}
 }
