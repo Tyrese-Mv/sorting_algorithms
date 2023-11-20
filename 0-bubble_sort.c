70bc5e6235f62a8f0e1fd691f1e7434b5fd9c8bb
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-20 13:30:13 +0200

    added task 0 fix 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,13 +17,13 @@ void bubble_sort(int *array, size_t size)
 		for (i = 0; i < size; i++)
 		{
 			swap = false
-			for (j = 0; j < (size - 1); j++)
+			for (j = 0; j < (size - i - 1); j++)
 			{
-				if (array[j] < array[j+1])
+				if (array[j] < array[j + 1])
 				{
 					temp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = temp;
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
 					swap = true;
 				}
 			}
