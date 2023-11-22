a3f8131bf0f2668b3c8295e509d966fcb338d848
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-22 10:43:04 +0200

    added task 0 fix 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,22 +7,37 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *prev = (*list), *current = (*list)->next, *fHead = (*list), *temp;
 
-	if ((*list)->next == NULL || *list == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 	while (current)
 	{
-		if (current->n > prev->n)
+		if (current->n >= prev->n)
 		{
 			prev = current;
 			current = current->next;
 		}
-		temp = fHead;
-		while (current->n > temp->next->n)
-			temp = temp->next;
-		prev->next = current->next;
-		current->next = temp->next;
-		temp->next = current;
-		current = prev->next;
-		print_list(*list);
+		else
+		{
+			temp = fHead;
+			if (current->n < temp->n)
+            {
+                prev->next = current->next;
+                current->next = fHead;
+                fHead = current;
+				*list = fHead;
+            }
+			else
+			{
+				while (temp->next != NULL && current->n > temp->next->n)
+					temp = temp->next;
+				prev->next = current->next;
+				current->next = temp->next;
+				temp->next = current;
+			}
+			current = prev->next;
+			print_list(*list);
+		}
+		
 	}
+	*list = fHead;
 }
