96d6483b291f884d6a7aaee4589a710b876da794
Tyrese-Mv <musawenkosi106@gmail.com>
2024-04-28 20:51:08 +0200

    added selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,1 +1,30 @@
+#include "sort.h"
 
+/**
+ * selection_sort - sorts array using selction sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_pos;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_pos = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_pos])
+				min_pos = j;
+		}
+		if (min_pos != i)
+		{
+			temp = array[i];
+			array[i] = array[min_pos];
+			array[min_pos] = temp;
+			print_array(array, size);
+		}
+	}
+}
